f016acb23be1a52b7b2e39cced02a108be3fe62e
Tameem <tameemisone786@gmail.com>
2023-10-13 23:21:01 -0400

    fixing the links, turns out we were trolling hard and forgot what / means

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,30 +2,25 @@ import React from "react";
 import "./Navbar.css";
 
 function Navbar(){
-    const nav = document.querySelector('.nav');
-    console.log(nav)
-    window.addEventListener('scroll', fixNav);
-
-    function fixNav (){
-        if(window.scrollY > nav.offsetHeight + 10){
-            nav.classList.add('active');
-        }else {
-            nav.classList.remove('active');
+    window.addEventListener('load', ()=> {
+        const nav = document.querySelector('.nav');
+        window.addEventListener('scroll', fixNav);
+        function fixNav (){
+            if(window.scrollY > nav.offsetHeight + 10){
+                nav.classList.add('active');
+            }else {
+                nav.classList.remove('active');
+            }
         }
-    }
-  }
-
-
+    })
+    
     return(
             <nav className="nav">
                 <div className="container">
                     <h1 className="logo"><a href="/index.html">OneWorld</a></h1>
                     <ul>
-                        <li><a href="#" class="current">Orphans</a></li>
-
-                        <li><a href="#" className="current">Orphans</a></li>
-
-                        <li><a href="#">Schools</a></li>
+                        <li><a href="/src/orphanDonationPage/orphan.html" className="current">Orphans</a></li>
+                        <li><a href="/src/school/schoolDonation.html">Schools</a></li>
                         <li><a href="#">War-Torn Countries</a></li>
                     </ul>
                     <button className="contactus">Contact us</button>
