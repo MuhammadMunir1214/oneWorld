a39b290a15cada30b116e5e25f162b48e40451d4
Tameem <tameemisone786@gmail.com>
2023-10-14 22:16:59 -0400

    Changing up designs of the navbar, footer, and of the headersection of landing page and aribs page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import "./Navbar.css";
 
-function Navbar(){
-    window.addEventListener('load', ()=> {
-        const nav = document.querySelector('.nav');
-        window.addEventListener('scroll', fixNav);
-        function fixNav (){
-            if(window.scrollY > nav.offsetHeight + 10){
-                nav.classList.add('active');
-            }else {
-                nav.classList.remove('active');
-            }
-        }
-    })
-    
-    return(
-            <nav className="nav">
-                <div className="container">
-                    <h1 className="logo"><a href="/index.html">OneWorld</a></h1>
-                    <ul>
-                        <li><a href="/src/orphanDonationPage/orphan.html" className="current">Orphans</a></li>
-                        <li><a href="/src/school/schoolDonation.html">Schools</a></li>
-                        <li><a href="/src/WarTorn/wartorn.html">War-Torn Countries</a></li>
-                    </ul>
-                    <button className="contactus">Contact us</button>
-                </div>
-            </nav>
-    );
+function Navbar() {
+  window.addEventListener("load", () => {
+    const nav = document.querySelector(".nav");
+    window.addEventListener("scroll", fixNav);
+    function fixNav() {
+      if (window.scrollY > nav.offsetHeight + 10) {
+        nav.classList.add("active");
+      } else {
+        nav.classList.remove("active");
+      }
+    }
+  });
+
+  return (
+    <nav className="nav">
+      <div className="container">
+        <h1>
+          <a className="logo" href="/index.html">OneWorld</a>
+        </h1>
+        <ul>
+          <li>
+            <a href="/src/orphanDonationPage/orphan.html" className="navLink">
+              Orphans
+            </a>
+          </li>
+          <li>
+            <a href="/src/school/schoolDonation.html" className="navLink">
+              Schools
+            </a>
+          </li>
+          <li>
+            <a href="/src/WarTorn/wartorn.html" className="navLink">
+              War-Torn Countries
+            </a>
+          </li>
+        </ul>
+        <button className="contactus"><a href="#contactusID">Contact us</a></button>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
