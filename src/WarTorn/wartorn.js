ae5c3ee9aa6726b1d21f2e56a821fd3077817e21
Sazassin <sazidomi2002@gmail.com>
2023-10-15 05:36:06 -0400

    Updated Wartorn Page

--- src/WarTorn/wartorn.js
+++ src/WarTorn/wartorn.js
@@ -11,4 +11,40 @@ document.addEventListener("DOMContentLoaded", function() {
         countries[i].style.left = x;
         countries[i].style.top = y;
     }
+
+    const target = document.getElementById('typeText');
+    
+    const observer = new IntersectionObserver(function(entries) {
+        if(entries[0].isIntersecting === true) {
+            target.classList.add('start-typing');  // Add a class to start the animation
+            observer.disconnect(); // Stop observing after the animation starts
+        }
+    }, { threshold: [0] });  // start when at least 0% of the target is visible
+
+    observer.observe(target);
 });
+
+/*USED UDEMY COURSE */
+
+const imgContainers = document.querySelectorAll(".image-container");
+
+imgContainers.forEach((container, index) => {
+  let idx = 0;
+  setInterval(() => {
+    idx++;
+    changeImage(container, idx);
+    if (idx >= container.querySelectorAll('img').length - 1) {
+      idx = -1; // reset index to -1 so that it becomes 0 on the next interval
+    }
+  }, 2000);
+});
+
+function changeImage(container, idx) {
+  const imageWidth = container.querySelector('img').offsetWidth;
+  container.style.transform = `translateX(${-idx * imageWidth}px)`;
+}
+
+
+
+
+
